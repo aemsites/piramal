977e521a880a22503e959aa96a34a6445058d713
asthabh23 <asthabhargava001@gmail.com>
2024-07-15 16:51:26 +0530

    fix: lint

--- blocks/hero-carousel/hero-carousel.js
+++ blocks/hero-carousel/hero-carousel.js
@@ -4,24 +4,24 @@
 export default function decorate(block) {
   // get the rows
   // first row contains the autoscroll flag
-  const [autoscroll, ...slides] = [...block.children]
+  const [autoscroll, ...slides] = [...block.children];
   // keep the flag as boolean
-  const autoscrollflag = autoscroll.children[0].innerText  === 'true';
+  // const isAutoscroll = autoscroll.children[0].innerText === 'true';
   autoscroll.remove();
 
   // loop through the slides
-  [...slides].forEach((slide,idx) => {
-    const [classes,description,image] = [...slide.children];
+  [...slides].forEach((slide) => {
+    const [classes, description, image] = [...slide.children];
 
-    //extract the classes list
+    // extract the classes list
     const classesText = classes.textContent.trim();
     const slideClasses = (classesText ? classesText.split(',') : []).map((c) => c && c.trim()).filter((c) => !!c);
     slideClasses.forEach((c) => slide.classList.add(c.trim()));
     classes.remove();
 
-    ///...
-    description.classList.add("description");
+    /// ...
+    description.classList.add('description');
 
-    image.classList.add("slide-image");
-  })
+    image.classList.add('slide-image');
+  });
 }
