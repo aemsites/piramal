bd1099eebf33d90e1481966f8bf8e580bb968d92
Mihai Indreias <mindreias@adobe.com>
2024-07-19 17:08:39 +0200

    cards variation 2 with CTA

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -5,11 +5,22 @@
 export default function decorate(block) {
   const children = [...block.children];
   children.forEach((card) => {
-    const [img, title, url] = card.querySelectorAll(':scope > div > *');
-
+    let img, title, url;
+    if (block.classList.contains('image-cards')) {
+      console.log(img, title, url);
+      const [bg, content] = [...card.children];
+      content.classList.add('content');
+      [title, url] = [...content.children];
+      bg.classList.add('card-img');
+      card.addEventListener('click', () => {
+        window.location.href = url.children[0].href;
+      });
+    } else {
+      [img, title, url] = card.querySelectorAll(':scope > div > *');
+      img.classList.add('card-img');
+    }
     card.classList.add('card');
 
-    img.classList.add('card-img');
     title.classList.add('title');
     url.classList.add('url');
   });
