2a31215b47a33cf405d54ae4fcbf36d3c808cd31
Mihai Indreias <mindreias@adobe.com>
2024-07-19 13:00:37 +0200

    media breakpoint and text alignment on desktop

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,16 +3,14 @@
  * @param {Element} block
  */
 export default function decorate(block) {
-  if (!block.classList.contains('variation-1')) {
-    const children = [...block.children];
-    children.forEach((card) => {
-      const [img, title, url] = card.querySelectorAll(':scope > div > *');
+  const children = [...block.children];
+  children.forEach((card) => {
+    const [img, title, url] = card.querySelectorAll(':scope > div > *');
 
-      card.classList.add('card');
+    card.classList.add('card');
 
-      img.classList.add('card-img');
-      title.classList.add('title');
-      url.classList.add('url');
-    });
-  }
+    img.classList.add('card-img');
+    title.classList.add('title');
+    url.classList.add('url');
+  });
 }
