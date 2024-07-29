a802f7d361fed92c2084b4480172829b2052b751
Mihai Indreias <mindreias@adobe.com>
2024-07-29 14:13:41 +0200

    slider

--- blocks/recommended-articles/recommended-articles.js
+++ blocks/recommended-articles/recommended-articles.js
@@ -1,9 +1,95 @@
 /**
  *
+ * @param {Element} div
+ * @param {Element} head
+ */
+
+const addControls = (div, head) => {
+  let selectedArticle = 0;
+  const prev = document.createElement('button');
+  const next = document.createElement('button');
+  prev.disabled = true;
+  const selectedArticleIndex = () => {
+    const articles = div.querySelectorAll('.article');
+    let closestIndex = 0;
+    let closestDistance = Infinity;
+
+    articles.forEach((article, index) => {
+      const distance = Math.abs(
+        article.offsetLeft - div.offsetLeft - div.scrollLeft,
+      );
+
+      if (distance < closestDistance) {
+        closestDistance = distance;
+        closestIndex = index;
+      }
+    });
+
+    selectedArticle = closestIndex;
+    prev.disabled = selectedArticle === 0;
+    next.disabled = div.scrollLeft + div.clientWidth === div.scrollWidth;
+  };
+  div.addEventListener('scroll', () => {
+    selectedArticleIndex();
+  });
+
+  // in head add another div with class controls
+  const leftChevron = document.createElement('img');
+  leftChevron.src = `${window.hlx.codeBasePath}/icons/chevron-left.svg`;
+  const rightChevron = document.createElement('img');
+  rightChevron.src = `${window.hlx.codeBasePath}/icons/chevron-left.svg`;
+
+  // rotate right chevron
+  rightChevron.style.transform = 'rotate(180deg)';
+
+  const controls = document.createElement('div');
+  // create 2 buttons elems
+  prev.append(leftChevron);
+  prev.classList.add('control', 'prev');
+
+  next.append(rightChevron);
+  next.classList.add('control', 'next');
+
+  controls.append(prev, next);
+
+  prev.addEventListener('click', () => {
+    // scroll to the selectedArticle - 1
+    const articles = div.querySelectorAll('.article');
+    const prevArticle = articles[selectedArticle - 1] || articles[0];
+    if (prevArticle) {
+      selectedArticle = Math.max(selectedArticle - 1, 0);
+      div.scrollTo({
+        left: prevArticle.offsetLeft - div.offsetLeft,
+        behavior: 'smooth',
+      });
+    }
+    selectedArticleIndex();
+  });
+
+  next.addEventListener('click', () => {
+    // scroll to the selectedArticle + 1
+    const articles = div.querySelectorAll('.article');
+    const nextArticle = articles[selectedArticle + 1];
+    if (nextArticle) {
+      div.scrollTo({
+        left: nextArticle.offsetLeft - div.offsetLeft,
+        behavior: 'smooth',
+      });
+      selectedArticle += 1;
+    }
+    selectedArticleIndex();
+  });
+
+  controls.classList.add('controls');
+
+  head.append(controls);
+};
+
+/**
+ *
  * @param {Element} block
  */
 export default function decorate(block) {
-  console.log(block);
   const [head, ...children] = block.querySelectorAll(':scope > div');
 
   children.forEach((child) => {
@@ -41,10 +127,12 @@ export default function decorate(block) {
   const div = document.createElement('div');
 
   div.classList.add('article-container');
+  addControls(div, head);
+
   head.classList.add('art-head');
+
   div.append(...children);
+
   block.append(div);
   block.prepend(head);
-
-  console.log(block);
 }
