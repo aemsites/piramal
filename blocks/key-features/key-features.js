7e664cbde3cc6584c78f28f57deddd4e90902a18
Mihai Indreias <mindreias@adobe.com>
2024-07-31 11:09:14 +0200

    fix styling rich text

--- blocks/key-features/key-features.js
+++ blocks/key-features/key-features.js
@@ -17,7 +17,6 @@ export default async function decorate(block) {
     fact.firstElementChild.append(text);
 
     title.classList.add('feature-title');
-    desc.classList.add('feature-desc');
   });
   await replacePlaceholders(block);
 }
