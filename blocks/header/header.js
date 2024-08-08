900999d800dbd0bae1b2721f8b9a495edb839939
Mihai Indreias <mindreias@adobe.com>
2024-08-08 15:45:12 +0200

    f

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -13,8 +13,8 @@ const wrapListUE = (navSection) => {
   const title = navSection.firstChild;
   const p = document.createElement('p');
   console.log(window.isEditor);
-  if(!window.isEditor)
-    return;
+  // if(!window.isEditor)
+  //   return;
   if (navSection.children.length !== 2) {
     console.log(navSection);
     if (navSection.firstElementChild.tagName === 'P') {
